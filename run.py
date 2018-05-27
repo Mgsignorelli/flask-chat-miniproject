0b4c0db7c0dabd6a81d793c5065902e1a50282f8
Micaela Signorelli <m.signorelli@gmail.com>
2018-05-27 09:46:26 +0000

    index redirects to chat

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 messages = []
@@ -10,18 +10,22 @@ def add_messages(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(message_dict)
+    messages.append(messages_dict)
 
 
 def get_all_messages():
-    """Get al of the messages and separate them by a `br`"""
+    """Get all of the messages and separate them by a `br`"""
     return messages
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
 
 
 @app.route('/<username>')
@@ -35,5 +39,6 @@ def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect(username)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
